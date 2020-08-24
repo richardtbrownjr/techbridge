d0dd2bcc830655493d17c6f70d4ee58985821708
Richard T. Brown Jr <richardtbrownjr@gmail.com>
2020-08-24 15:13:26 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# READ ME
+# Aug 18, 2020 i put out this short video - https://youtu.be/fDWB4lf1Uvk
+# i said I push it to Git hub   to show user input and print statement.
---
+++ techbridge01.py
@@ -0,0 +1,10 @@
+# tech bridge user inputs and print statement
+
+userinputname = input("What is your name? ")
+userinputteam = input("Who is your team? ")
+userinputlastchampionship = int(input("When did they win last? "))
+print()
+print("Thanks", userinputname, "Your team is - ", userinputteam)
+print("They won their last championship in ", userinputlastchampionship)
+print()
+print()
